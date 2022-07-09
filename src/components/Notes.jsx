28bcfe82a592bd871b3288862664b3a22bb6b1c9
Shyam ji Gupta <91778264+Shyamjizoyo@users.noreply.github.com>
2022-07-09 08:44:42 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1> This is react bootcamp </h1>
+      <p>Hey bros this is shyam ji and i love printing hello world lol </p>
+    </div>
+  );
+}
+
+export default Notes;
